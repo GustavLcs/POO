a581970e74c4caa5f3b80a10d24adf6e1a60fd04
Gustavo Lucas <143631931+GustavLcs@users.noreply.github.com>
2024-04-16 23:44:35 -0300

    Update Main.java

---
+++ List1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      Calculadora c1 = new Calculadora();
+      c1.captureInformation();
+      c1.calculate();
+      c1.show();
+    }
+  }
